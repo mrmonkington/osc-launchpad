938b890f2a60f4ac8ec1c5c5e775afac38d1c784
Mark Kennedy <mark@gamer-network.net>
2015-01-23 22:45:55 +0000

    custom RGB buttons respond to bitwig init sends

--- launchpad.py
+++ launchpad.py
@@ -3,6 +3,31 @@ import sys, threading
 import liblo
 from gi.repository import Gtk
 from gi.repository import Gdk
+import re
+
+DEBUG=True
+
+class OSCServer(liblo.ServerThread):
+    def __init__(self, app):
+        liblo.ServerThread.__init__(self, 9000)
+        self.app = app
+
+    def parse_rgb(self, rgbstr):
+        return [float(x) for x in re.match("RGB\(([\.0-9]+),([\.0-9]+),([\.0-9]+)\)", rgbstr).groups()]
+
+    @liblo.make_method(None, None)
+    def fallback(self, path, args):
+        if DEBUG:
+            print "received unknown message '%s'" % path
+            print args
+        chunks = path.split("/")
+        if len(chunks) == 4:
+            if chunks[1] == 'track' and chunks[3] == 'color':
+                track = int(chunks[2])
+                for clip in range(1,8+1):
+                    color = self.parse_rgb(args[0])
+                    self.app.gui.leds[track][clip].color = color
+                    self.app.gui.leds[track][clip].queue_draw()
 
 class App(object):
     def __init__(self):
@@ -16,6 +41,13 @@ class App(object):
         self.gui.connect('destroy', lambda quit: Gtk.main_quit())
 
     def run(self):
+        try:
+            server = OSCServer(self)
+        except ServerError, err:
+            print str(err)
+            sys.exit()
+
+        server.start()
         Gtk.main()
 
     def quit(self, *arg):
@@ -34,11 +66,57 @@ class OSCButton(Gtk.Button):
         self.osc = osc_target
         self.msg = msg
         self.connect("touch-event", self.pressed)
+        #color = Gdk.color_parse('#234fdb')
+        #self.modify_bg(Gtk.StateType.PRELIGHT, color)
         #self.set_double_buffered()
 
     def pressed(self, tgt, ev):
         if ev.touch.type == Gdk.EventType.TOUCH_BEGIN:
             liblo.send(self.osc, self.msg)
+
+class ColorOSCButton(Gtk.Widget):
+    __gtype_name__ = 'ColorOSCButton'
+
+    def __init__(self, label, osc_target, msg):
+        Gtk.Widget.__init__(self)
+        self.osc = osc_target
+        self.msg = msg
+        self.connect("touch-event", self.pressed)
+        self.color = (0.95, 0.95, 0.95)
+        self.set_size_request(40, 40)
+
+    def pressed(self, tgt, ev):
+        if ev.touch.type == Gdk.EventType.TOUCH_BEGIN:
+            liblo.send(self.osc, self.msg)
+
+    def do_draw(self, cr):
+        # paint background
+        cr.set_source_rgb(*list(self.color))
+        #cr.paint()
+        allocation = self.get_allocation()
+        cr.rectangle(0, 0, allocation.width, allocation.height)
+        cr.fill()
+
+    def do_realize(self):
+        allocation = self.get_allocation()
+        attr = Gdk.WindowAttr()
+        attr.window_type = Gdk.WindowType.CHILD
+        attr.x = allocation.x
+        attr.y = allocation.y
+        attr.width = allocation.width
+        attr.height = allocation.height
+        attr.visual = self.get_visual()
+        attr.event_mask = self.get_events() | Gdk.EventMask.EXPOSURE_MASK | Gdk.EventMask.TOUCH_MASK
+        WAT = Gdk.WindowAttributesType
+        mask = WAT.X | WAT.Y | WAT.VISUAL
+
+        window = Gdk.Window(self.get_parent_window(), attr, mask);
+        self.set_window(window)
+        self.register_window(window)
+
+        self.set_realized(True)
+        window.set_background_pattern(None)
+
 
 class Gui(Gtk.Window):
     def __init__(self, app):
@@ -49,17 +127,23 @@ class Gui(Gtk.Window):
 
         self.table = Gtk.Table(8, 8, True)
         self.add(self.table)
+        self.leds = [[False for x in range(8+1)] for x in range(8+1)] 
 
         for track in range(1,8+1):
             for clip in range(1,8+1):
-                self.table.attach_defaults(
-                    OSCButton(
-                        "%i:%i" % (track, clip),
-                        self.app.osc_target,
-                        liblo.Message("/track/%i/clip/%i/launch" % (track, clip))
-                    ),
+                self.leds[track][clip] = ColorOSCButton(
+                    "%i:%i" % (track, clip),
+                    self.app.osc_target,
+                    liblo.Message("/track/%i/clip/%i/launch" % (track, clip))
+                )
+                self.table.attach(
+                    self.leds[track][clip],
                     track-1,track,
-                    clip-1,clip
+                    clip-1,clip,
+                    Gtk.AttachOptions.EXPAND,
+                    Gtk.AttachOptions.EXPAND,
+                    10,
+                    10
                 )
 
         self.show_all()
@@ -70,3 +154,6 @@ class Gui(Gtk.Window):
 if __name__=='__main__':
     app = App()
     app.run()
+
+
+
